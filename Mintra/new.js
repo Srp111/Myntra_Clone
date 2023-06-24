8693ad315c79c20c13e70caa7e1b839807f518b4
saurabh patel <srp10042000@gmail.com>
2023-06-24 15:04:51 +0530

    comitting Myntra clone

---
+++ Mintra/new.js
@@ -0,0 +1,23 @@
+// let list1=document.querySelectorAll("div>ol>li")
+// list1.forEach(x=> {
+//     x.addEventListener("mouseover",()=>{
+//         x.style.background="red"
+//         x.style.cursor="pointer"
+//         console.log(x);
+        
+//     })
+// });
+// let a=document.getElementById("a")
+// let b=document.getElementById("b")
+// let c=document.getElementById("c")
+// let d=document.getElementById("d")
+// let e=document.getElementById("e")
+// let f=document.getElementById("f")
+
+// let array1=[a,b,c,d,e,f]
+// array1.forEach(x => {
+//     x.addEventListener("mouseoevr",()=>{
+//         x.style.background="red"
+//     })
+// });
+
